5a8b5712ed73b1afac706be88a8aa6cdf2d91f7e
Steklish <anton.kozlov.mailbox@gmail.com>
2025-02-22 11:44:26 +0300

    First analysys

---
+++ bot_instance.py
@@ -0,0 +1,4 @@
+import telebot
+import prefs
+
+bot = telebot.TeleBot(prefs.TG_API)
--- main.py
+++ main.py
@@ -6,16 +6,121 @@ import prefs
 import json
 from media_handler import *
 import tools
-import datetime  # Add this import
-import sys  # Add this import
+import datetime
 
 
 # Telegram creds
-bot = telebot.TeleBot(prefs.TG_API)
+from bot_instance import bot 
 bot.send_message(prefs.chat_to_interact, "👩🏻‍🦼👩🏻‍🦼👩🏻‍🦼💨 host started 👩🏻‍🦼👩🏻‍🦼👩🏻‍🦼💨")
 
+def smart_response():
+    try:
+        print(GREEN)
+        print("Smart message launched", RESET)
+        current_datetime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        sys_m = {
+                'role': 'system',
+                'content': f"""настоящие время и дата {current_datetime}
+                {prefs.system_msg}
+                """
+            }
+        current_datetime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        
+        msgs = []
+        with open("static_storage/conversation.json", "r", encoding="utf-8") as f:
+            msgs = json.loads(f.read())
+        # print(json.dumps([sys_m, *msgs], indent=4, ensure_ascii=False))
+        # print(YELLOW, json.dumps([sys_m, *msgs], ensure_ascii=False), RESET)
+        
+        resp = OR_client.chat.completions.create(
+            model=prefs.MODEL,
+            messages = [sys_m, *msgs],
+            tool_choice='auto',
+            tools=tools.TOOLS,
+            parallel_tool_calls=True,
+            stream=False
+        )
+        print(resp)
+        
+        # if resp.id is None:
+        #     bot.send_message(
+        #                 prefs.TST_chat_id,
+        #                 "```Error_from_AI_client```", parse_mode="Markdown"
+        #             )
+        #     return
+        
+        plain_text = ''
+        
+        plain_text = resp.choices[0].message.content
+        func_raw = resp.choices[0].message.tool_calls
+        
+        # print(f"{RED}{plain_text}{RESET}")
+        
+        if func_raw is not None:
+            for call in func_raw:
+                func_name = call.function.name
+                func_params = call.function.arguments
+                call_id = call.id
+                call_type = call.type
+                call_index = call.index
+                res = None
+                msgs.append(
+                                {
+                                    "role": "assistant",
+                                    "function_call": {
+                                        'tool_call_id' : str(call_id),
+                                        "name" : str(func_name),
+                                        # "type" : str(call_type),
+                                        # "index" : str(call_index),
+                                        # "arguments" : str(func_params)
+                                    }
+                                }
+                            )
+                
+        
+        
+        
+                # !FUNCTION EXECUTION
+        
+                try:
+                    res = tools.execute_tool(func_name, func_params)
+                    if func_raw[0].function.name == "send_group_message":
+                        continue
+                    if res is None: res = 'no return'
+                    shoul_call_once_more = True
+                    #success log    
+                    
+                
+                    msgs.append(
+                                    {
+                                        'role': 'function',
+                                        "name" : str(func_name),
+                                        'output': str(res),
+                                        # "type" : str(call_type),
+                                        # "index" : str(call_index),
+                                        'tool_call_id' : str(call_id)
+                                    }
+                                )
+                    
+                    with open("static_storage/conversation.json", "w", encoding="utf-8") as f:
+                        f.write(json.dumps(msgs, indent=4, ensure_ascii=False))
+                    force_response()
+                    smart_response()
+                except Exception as e: 
+                    print(e)
+                    bot.send_message(
+                            prefs.TST_chat_id,
+                            "```Cannot_execute_tool " + str(e) + "```", parse_mode="Markdown"
+                        )
+        else:
+            print(YELLOW, "deciced to stay silent...", RESET)
+    except Exception as e:
+        bot.send_message(
+            prefs.TST_chat_id,
+            "```Cannot_send_response_very_BAD  (smart response)" + str(e) + "```", parse_mode="Markdown"
+        )    
 
-def force_responce():
+def force_response():
     try:
         print("Forced message")
         current_datetime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
@@ -38,6 +143,7 @@ def force_responce():
             messages = [sys_m, *msgs],
             tool_choice='auto',
             tools=tools.TOOLS,
+            parallel_tool_calls=True,
             stream=False
         )
         print(resp)
@@ -50,7 +156,6 @@ def force_responce():
         #     return
         
         plain_text = ''
-        shoul_call_once_more = False
         
         plain_text = resp.choices[0].message.content
         func_raw = resp.choices[0].message.tool_calls
@@ -95,11 +200,11 @@ def force_responce():
                                 {
                                     "role": "assistant",
                                     "function_call": {
-                                        # 'tool_call_id' : str(call_id),
+                                        'tool_call_id' : str(call_id),
                                         "name" : str(func_name),
                                         # "type" : str(call_type),
                                         # "index" : str(call_index),
-                                        "arguments" : str(func_params)
+                                        # "arguments" : str(func_params)
                                     }
                                 }
                             )
@@ -124,13 +229,13 @@ def force_responce():
                                         'output': str(res),
                                         # "type" : str(call_type),
                                         # "index" : str(call_index),
-                                        # 'tool_call_id' : call_id
+                                        'tool_call_id' : str(call_id)
                                     }
                                 )
                     
                     with open("static_storage/conversation.json", "w", encoding="utf-8") as f:
                         f.write(json.dumps(msgs, indent=4, ensure_ascii=False))
-        
+                    force_response()
                 except Exception as e: 
                     print(e)
                     bot.send_message(
@@ -140,7 +245,7 @@ def force_responce():
     except Exception as e:
         bot.send_message(
             prefs.TST_chat_id,
-            "```Cannot_send_response_very_BAD " + str(e) + "```", parse_mode="Markdown"
+            "```Cannot_send_response_very_BAD (force response) " + str(e) + "```", parse_mode="Markdown"
         )
         
         
@@ -166,12 +271,12 @@ def process_any_msg(message:telebot.types.Message):
         }, indent=4, ensure_ascii=False)
     }
     msgs.append(msg)
-    if len(msgs)  > 50:
+    if len(msgs)  > prefs.history_depth:
         msgs = msgs[10:]
     with open("static_storage/conversation.json", "w", encoding="utf-8") as f:
         f.write(json.dumps(msgs, indent=4, ensure_ascii=False))
-        
-    force_responce()
+    smart_response()  
+    # force_response()
   
 @bot.message_handler(content_types=['document', 'photo', 'video', 'audio', 'voice'], func=lambda message: str(message.chat.id) == prefs.chat_to_interact)
 def handle_files(message):
@@ -237,7 +342,7 @@ def handle_files(message):
             "```GENERAL_Error: General_error_in_handle_files " + str(e) + "```", parse_mode="Markdown"
         )
         
-    force_responce()
+    force_response()
 try:    
     bot.polling()
 except Exception as e:
