4d3df9ac73c11825d3d97931d27db2130be53f64
Steklish <anton.kozlov.mailbox@gmail.com>
2025-02-26 20:26:28 +0300

    wrong creds 02

--- conf_info.py
+++ conf_info.py
@@ -5,7 +5,7 @@ TG_API = '7587368548:AAFuBJQ31-HpYw1HwxTdnVr70j2awg88iU4'
 
 
 #skls
-my_op_key="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"
+my_op_key="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"
 
 sn_op_key="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"
 
